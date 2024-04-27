b9d5a8f5d6867b7d1758a487fc0a57cba2e87f97
StefanoMarinelli <stefano.marinelli@marconirovereto.it>
2024-04-27 14:23:42 +0200

    Finish

--- src/main/java/it/marconi/loginpage/controllers/Logincontroller.java
+++ src/main/java/it/marconi/loginpage/controllers/Logincontroller.java
@@ -1,24 +1,42 @@
 package it.marconi.loginpage.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
-import it.marconi.loginpage.domains.User;
+import it.marconi.loginpage.domains.NewUser;
 
 @Controller
 @RequestMapping("/utente")
 public class Logincontroller {
-    
-    @GetMapping("/login")
-    public ModelAndView login(){
-        return new ModelAndView("login-page").addObject(new User());
+
+    @GetMapping
+    public ModelAndView page(@RequestParam(name = "type") String type) {
+        if (type.equals("new")) {
+            return new ModelAndView("registration-page").addObject(new NewUser());
+        } else if (type.equals("login")) {
+            return new ModelAndView("login-page").addObject(new NewUser());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Errore nel caricamento");
+        }
     }
 
-    @GetMapping("/new")
-    public ModelAndView newUser(){
-        return new ModelAndView("newuser-page").addObject(new User());
+    @PostMapping("/birillo")
+    public ModelAndView registrazione(@RequestParam String nickname, /* @RequestParam String email, */
+            @RequestParam String pwd) {
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("recap-page");
+        mv.addObject("nickname", nickname);
+        // mv.addObject("email", email);
+        mv.addObject("pwd", pwd);
+
+        return mv;
     }
 
 }
--- src/main/java/it/marconi/loginpage/domains/NewUser.java
+++ src/main/java/it/marconi/loginpage/domains/NewUser.java
@@ -7,9 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
-    
-    
+public class NewUser {
+
     private String nickname;
 
     private String email;
